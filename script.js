c83e0137f397c98c1b65b874214d6e68d311a652
Golden-Exp <pranavkrishnan01@gmail.com>
2024-12-06 16:56:09 +0000

    fixed some bugs

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const buttons = document.querySelectorAll("button")
 const display = document.querySelector("#display")
+const op_disp = document.querySelector("#op_disp")
 let no_point = true
 let a = ""
 let b = ""
 let op = ""
+let un = ""
 function operate(a, b, op) {
     if (op == "+") {
         return a + b
@@ -31,6 +33,7 @@ buttons.forEach((button) => {
             if (display.classList.contains("result")) {
                 display.classList.remove("result")
                 display.innerHTML = ""
+                op_disp.textContent = ""
             }
             if (display.textContent.length >= 13) {
                 display.innerHTML = display.innerHTML
@@ -49,10 +52,16 @@ buttons.forEach((button) => {
                 if (!no_point){
                     b = parseFloat(display.textContent)
                 }
+                un = ""
+                if (operate(a, b, op) < 0) {
+                    un = "-"
+                }
                 display.textContent = operate(a, b, op)
+                op_disp.textContent = "="
                 a = ""
                 b = ""
                 op = ""
+                
             }
             else {
                 if (display.textContent == "" && "dividemul".includes(button.id))
@@ -66,15 +75,22 @@ buttons.forEach((button) => {
                     }
                     op = button.textContent
                     display.classList.add("result")
+                    op_disp.textContent = op
+                    un = ""
                 }
-                else if(a != "" && b == "") {
+                else if(a != "" && b == "" && op != "") {
                     b = parseInt(display.textContent)
                     if (!no_point){
                         b = parseFloat(display.textContent)
                     }
                     a = operate(a, b, op)
+                    un = ""
+                    if (a < 0) {
+                        un = "-"
+                    }
                     display.textContent = a
                     op = button.textContent
+                    op_disp.textContent = op
                     display.classList.add("result")
                 }
             }
@@ -90,11 +106,41 @@ buttons.forEach((button) => {
         }
         else if(button.id == "back")
         {
-            display.textContent = display.textContent.slice(0, -1)
+            if (a != "" && op != "") {
+                op = ""
+                op_disp.textContent = ""
+                a = ""
+                display.classList.remove("result")
+            }
+            else if(display.textContent[display.textContent.length - 1] == ".") {
+                no_point = true
+                display.textContent = display.textContent.slice(0, -1)
+                display.classList.remove("result")
+            }
+            else {
+                display.textContent = display.textContent.slice(0, -1)
+                display.classList.remove("result")
+            }
         }
         else if(button.id == "point" && no_point) {
             display.textContent = display.textContent.concat(".")
             no_point = false
+            display.classList.remove("result")
+        }
+        else if (button.id == "unary") {
+            if (display.textContent == "") {}
+            else if (un == "" ) {
+                display.textContent = "-".concat(display.textContent)
+                un = "-"
+            }
+            else if (un == "-"){
+                display.textContent = "+".concat(display.textContent.slice(1))
+                un = "+"
+            }
+            else {
+                display.textContent = "-".concat(display.textContent.slice(1))
+                un = "-"
+            }
         }
     })
 });
